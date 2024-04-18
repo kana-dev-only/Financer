bc7d85f1b346a13b4b4c3d07dea3af2d3eb4c120
Nikto_888 <amanbaikanat99@gmail.com>
2024-04-18 10:26:33 +0500

    final

---
+++ FinanceManager/Converters/PeriodToBoolConverterForMonth.cs
@@ -0,0 +1,21 @@
+﻿using FinanceManager.Core;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace FinanceManager.Converters
+{
+    class PeriodToBoolConverterForMonth : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((Period)value == Period.Month) return true;
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
---
+++ FinanceManager/Core/ApplicationContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using FinanceManager.Model;
+
+namespace FinanceManager.Core
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Currency> Currency { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+        public string DbPath { get; }
+
+        public ApplicationContext()
+        {
+            DbPath = "FinanceManager.db";
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+    }
+}
